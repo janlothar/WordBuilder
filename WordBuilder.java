74bfc1dea9f4f741a4977d8127d150dc6aaf860d
Jan Warncke <janlothar@gmail.com>
2015-12-17 18:22:54 -0700

    Add lettersInWord()

--- WordBuilder.java
+++ WordBuilder.java
@@ -15,11 +15,15 @@ import java.util.Scanner;
 public class WordBuilder{
 	
 	public static void main (String[] args) throws IOException{
-		WordBuilder dictionary = new WordBuilder();
-		List<String> lines = dictionary.readFile(FILE_NAME);
-		Scanner keyboard = new Scanner(System.in);
-		String userInput = keyboard.nextLine();
-		System.out.print(dictionary.find(userInput, lines));
+		
+		WordBuilder dictionary = new WordBuilder();							//create non-static object
+		List<String> lines = dictionary.readFile(FILE_NAME);				//create List to hold lines from FILE_NAME
+		//Scanner keyboard = new Scanner(System.in);							//Scanner for keyboard input
+		//String userInput = keyboard.nextLine();								//user input as string in userInput
+		
+		String[] test1 = "hello".split("");
+		String[] test2 = "hlop".split("");
+		System.out.println(dictionary.lettersInWord(test2, test1));
 	}
 	
 	final static String FILE_NAME = "dictionary.txt";
@@ -32,10 +36,17 @@ public class WordBuilder{
 		return Files.readAllLines(path, ENCODING);
 	}
 	
-	//Prints contents of list to console line-by-line 
+	//Prints contents of List to console line-by-line 
 	public void print(List toPrint){
 		for(int i=0;i<toPrint.size();i++){
 			System.out.println(toPrint.get(i));
+		}
+	}
+	
+	//Prints contents of String[] to console line-by-line 
+	public void print(String[] toPrint){
+		for(int i=0;i<toPrint.length;i++){
+			System.out.println(toPrint[i]);
 		}
 	}
 	
@@ -47,8 +58,40 @@ public class WordBuilder{
 		return -1;
 	}
 	
-	public String findContains(String contains){
+	//Find words containing all letters entered
+	public List findContains(String contains, List inList){
+		/* String[] toFind = contains.split("");
+		String[] temp;
+		List toReturn;
+		for(int i=0;i<inList.size();i++){
+			temp = inList.get(i).split("");
+			for(int x=0;x<temp.length;x++){
+				for(int y=0;y<toFind.length;y++){
+					if(temp[x].equals(toFind[y])){
+						break;
+					}
+					
+				}
+			}
+		} */
 		return null;
+	}
+	
+	//returns true if word contains all characters in letters
+	public boolean lettersInWord(String[] letters, String[] word){
+		for(int x=0;x<letters.length;x++){
+			for(int y=0;y<word.length;y++){
+				if(letters[x].equals(word[y])){
+					System.out.printf("\nfound--- letters[%d]: %s = word[%d]: %s", x, letters[x], y, word[y]);
+					break;
+				}
+				else if(y==word.length-1){
+					System.out.printf("\nreturning false letter[%d]: %s has no match in %s", x, letters[x], Arrays.toString(word));
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	
 }

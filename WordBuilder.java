04f4cb3bcb145ef81f356b3a7012792c4cd9cb27
Jan Warncke <janlothar@gmail.com>
2017-10-10 13:55:56 -0600

    Remove unused imports

--- WordBuilder.java
+++ WordBuilder.java
@@ -1,19 +1,14 @@
 //Written by Jan Warncke
 //Reading from text files heavily based on the tutorial @ http://www.javapractices.com/topic/TopicAction.do?Id=42
 
-//starting working on CPSC501 assignment #1
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class WordBuilder{
 		

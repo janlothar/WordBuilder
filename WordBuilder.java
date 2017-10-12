404baac7ebb167a31d971911adcf857cd22e21cc
Jan Warncke <janlothar@gmail.com>
2017-10-12 14:31:59 -0600

    Combine Method and Rename Method refactoring
    Combined checkLettersContains() and checkLettersAnagram() since they
    were the same method

--- WordBuilder.java
+++ WordBuilder.java
@@ -54,29 +54,11 @@ public class WordBuilder{
 	public List<String> findContains(String contains){
 		List<String> toReturn = new ArrayList<String>();						//List to return that will be empty if no matching words are found
 		for(int i=0;i<dictionary.size();i++){										//Go through each word in dictionary
-			if (checkLettersContains(contains.split(""), dictionary.get(i).split(""))){	
+			if (checkLetters(contains.split(""), dictionary.get(i).split(""))){	
 				toReturn.add(dictionary.get(i));									//add word to toReturn if matches
 			}
 		}
 		return toReturn;
-	}
-	
-	//[ARRAY] returns true if word contains all characters in letters
-	public boolean checkLettersContains(String[] letters, String[] word){
-		for(int x=0;x<letters.length;x++){					//go through each letter for which a match must be found
-			for(int y=0;y<word.length;y++){					//go through each letter of the word
-				if(letters[x].equals(word[y])){
-					//System.out.printf("\nfound--- letters[%d]: %s = word[%d]: %s", x, letters[x], y, word[y]);							//Debug: Check why a match on each letter
-					word[y]="0";							//if match is found then that letter from word is removed by writing over with "0"
-					break;									//no other matches need to be found so look at next letter
-				}
-				else if(y==word.length-1){
-					//System.out.printf("\nreturning false letter[%d]: %s has no match in %s", x, letters[x], Arrays.toString(word));		//Debug: Check why not a match
-					return false;							//if no match for that letter is found in word then word does not contain all letters
-				}
-			}
-		}
-		return true;
 	}
 	
 	//Return List of words that can be built from given letters
@@ -110,14 +92,14 @@ public class WordBuilder{
 	public List<String> findAnagram(String contains){
 		List<String> toReturn = new ArrayList<String>();						//List to return that will be empty if no matching words are found
 		for(int i=0;i<dictionary.size();i++){										//Go through each word in dictionary
-			if (contains.length() == dictionary.get(i).length() && checkLettersAnagram(contains.split(""), dictionary.get(i).split(""))){	//For anagram word must be as long as input string
+			if (contains.length() == dictionary.get(i).length() && checkLetters(contains.split(""), dictionary.get(i).split(""))){	//For anagram word must be as long as input string
 				toReturn.add(dictionary.get(i));									//add word to toReturn if matches
 			}
 		}
 		return toReturn;
 	}
 	
-	public boolean checkLettersAnagram(String[] letters, String[] word){
+	public boolean checkLetters(String[] letters, String[] word){
 		for(int x=0;x<letters.length;x++){					//go through each letter for which a match must be found
 			for(int y=0;y<word.length;y++){					//go through each letter of the word
 				if(letters[x].equals(word[y])){

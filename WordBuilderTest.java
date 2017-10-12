e554c19f01c178912444caca6e2e0c76dde82ae5
Jan Warncke <janlothar@gmail.com>
2017-10-12 13:40:24 -0600

    Add single test cases for findBuild() findContains() and findAnagram()

--- WordBuilderTest.java
+++ WordBuilderTest.java
@@ -2,11 +2,35 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WordBuilderTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindBuild() throws IOException {
+		WordBuilder testFindBuild = new WordBuilder();
+		String[] toTestList1 = {"ad", "ads", "as", "fa", "fad", "fads", "fas", "sad"};
+		List<String> testList1 = new ArrayList<String>(Arrays.asList(toTestList1));
+		assertEquals(testList1, testFindBuild.findBuild("asdf"));
+	}
+	
+	@Test
+	public void testFindContains() throws IOException {
+		WordBuilder testFindContains = new WordBuilder();
+		String[] toTestList1 = {"fatheadedness", "fatheadednesses"};
+		List<String> testList1 = new ArrayList<String>(Arrays.asList(toTestList1));
+		assertEquals(testList1, testFindContains.findContains("asdfasd"));
+	}
+	
+	@Test
+	public void testFindAnagram() throws IOException {
+		WordBuilder testFindAnagram = new WordBuilder();
+		String[] toTestList1 = {"opts", "post", "pots", "spot", "stop", "tops"};
+		List<String> testList1 = new ArrayList<String>(Arrays.asList(toTestList1));
+		assertEquals(testList1, testFindAnagram.findAnagram("opst"));
 	}
 
 }
